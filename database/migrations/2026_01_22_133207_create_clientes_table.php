1a3e5cec1a2c7db69cfb19568b5e9aedbe4a046a
chicleti2450 <leticiadomigs2450@gmail.com>
2026-01-22 15:38:53 -0300

    Implementação CRUD de Clientes

--- database/migrations/2026_01_22_133207_create_clientes_table.php
+++ database/migrations/2026_01_22_133207_create_clientes_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('clientes', function (Blueprint $table) {
             $table->id();
             $table->string('nome');
-            $table->integer('cpf');
+            $table->integer('cpf')->unique();
             $table->integer('idade');
             $table->timestamps();
         });
